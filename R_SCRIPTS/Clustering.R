83542c9be7e7795ba5ddc3a7cc76fbeb617d7938
Fergus <68643229+NotRipley@users.noreply.github.com>
2026-04-05 17:18:08 +0100

    commit after most of sunday

--- R_SCRIPTS/Clustering.R
+++ R_SCRIPTS/Clustering.R
@@ -1,8 +1,92 @@
+# PART 3: CLUSTERING
+source(file = "./R_SCRIPTS/Functions.R")
+
+# LIBRARIES
+library(tidyverse) # misc
+library(cluster) # kmeans, hierarchical, silhouette
+library(factoextra) # hierarchical
+library(mclust) # ari
+library(fpc) # calinhara
+
+# DATA
+D <- load_data()
+
 # CLUSTER SAMPLES WITH AN APPROPRIATE DISTANCE METRIC 
+# We will use squared euclidean distance, the one used as a default in a lot
+# of algorithms
 
+# =====================================================
 # IDENTIFY THE MOST APPROPRIATE NUMBER OF CLUSTERS (ELBOW OR GAP)
+# =====================================================
 
+# N.B. we are carrying forward only five PCs from our PC section.
+
+P <- prcomp(D$data)
+embeddings <- P$x[, 1:npcs]
+
+k <- estimate_k(embeddings, FUN = kmeans, b = 100)
+
+#Where now we've added this to Functions.R
+
+# =====================================================
 # COMPARE CLUSTERING RESULTS ON TWO DIFFERENT METHODS
+# =====================================================
 
+# =====================================================
+# KMEANS
+# =====================================================
+
+K_clustering <- run_kmeans(embeddings, K = K_kmeans)
+
+ggplot(data = embeddings) +
+  geom_point(mapping = aes(x = PC1, y = PC2, colour = as.factor(kmobject$cluster)))
+
+# =====================================================
+# HIERARCHICAL
+# =====================================================
+
+# Check which method yields the highest "agglomerative coefficient", this 
+# being "ward".
+(sapply(m, function(m_val) hierarchical_check_methods(m_val, df = embeddings)))
+
+# Choose an optimal K for use with hierarchical clustering, this also being 6. 
+k <- estimate_k(embeddings, hcut)
+
+# Run the clustering.
+H_clustering <- run_hierarchical(matrix = embeddings, method = method_choice, 
+                                 K = K_hier)
+
+# =====================================================
 # DISCUSS WHETHER WE CAN DESCRIBE OUR CLUSTERS IN TERMS OF THE COVARIATES: TIME.
 # TREATMENT, REPLICATES
+# =====================================================
+
+# This is adding the partitions onto the df, clustering and then colouring by 
+# each of our covariates. 
+
+# KMeans first
+table(K_clustering$cluster, D$treatments)
+table(K_clustering$cluster, D$times)
+
+chisq.test(table(K_clustering$cluster, D$treatments))
+chisq.test(table(K_clustering$cluster, D$times))
+
+# Hierarchical next - need to cuttree first.
+table(H_clustering$cluster, D$treatments)
+table(H_clustering$cluster, D$times)
+
+chisq.test(table(H_clustering$cluster, D$treatments))
+chisq.test(table(H_clustering$cluster, D$times))
+
+# Wow, these are really similar! Are they the same partition?
+table(H_clustering$cluster, K_clustering$cluster)
+# Yes, the table is decoupled. To really force the point:
+HK_ari <- mclust::adjustedRandIndex(H_clustering$cluster, K_clustering$cluster)
+
+# =====================================================
+# EVALUATE CLUSTERING PERFORMANCE
+# =====================================================
+
+
+
+
--- R_SCRIPTS/Functions.R
+++ R_SCRIPTS/Functions.R
@@ -1,4 +1,13 @@
+seed <- 1618
+
+##########
+# PART 1 #
+#########
+
+
 load_data <- function(){
+  # Loads data from named file and returns as a list.
+  # -------------------------------------------------
   
   e <- new.env()
   load(file = "./Assignment3_Data.RData", envir = e)
@@ -17,3 +26,147 @@ treatment_colours <- c(
   "Lisinopril" <- "steelblue",
   "Amlodipine" <- "tomato"
 )
+
+##########
+# PART 2 #
+##########
+
+# Set centrally the number of principal components to carry forward
+npcs <- 5
+
+##########
+# PART 3 #
+##########
+
+
+estimate_k <- function(matrix, FUN, scale = FALSE, b = 100, kmax = 20, itermax = 20, 
+                       nstart = 25) {
+  # Takes a value/counts matrix and estimates the best number of clusters
+  # via a Gap statistic method, using the cluster::clusGap function. Returns
+  # list comprised of the K recommendation and a visualisation, via 
+  # fviz_gap_stat().
+  # ----------------------------------------------------------------------
+  # matrix - matrix of values on which to perform KMeans clustering. 
+  # FUN - function to perform. kmeans or hcut for example. 
+  # scale - logical on whether to scale as part of this step. FALSE by default.
+  # other parameters - passed to clusGap function. 
+  
+  if (!is.logical(scale) | scale == FALSE) {
+    cat("Not scaling matrix... \n")
+  } else if (scale == TRUE) {
+    matrix <- scale(matrix)
+    cat("Scaling matrix... \n")
+  }
+  
+  clusGapcall <- clusGap(matrix,
+                         FUNcluster = FUN,
+                         nstart = 25,
+                         iter.max = 20,
+                         K.max = kmax,
+                         B = b)
+  
+  k_val <- maxSE(clusGapcall$Tab[, "gap"], clusGapcall$Tab[, "SE.sim"], 
+                 method = "globalmax")
+  
+  cat("clusGap gives ", k_val, " as our K estimate. \n")
+  
+  viz <- fviz_gap_stat(clusGapcall)
+  
+  return(list("k_val" = k_val,
+              "viz" = viz))
+  
+}
+
+# Set centrally the number of clusters we want to use, after running the above
+# with each method.
+K_kmeans <- 6
+K_hier <- 6
+
+
+
+run_kmeans <- function(matrix, K, itermax = 20, nstart = 25){
+  # Runs KMeans clustering on a matrix with given K.
+  # -------------------------------------------------
+  # matrix - matrix to cluster on.
+  # K - number of centroids.
+  # other params - to pass to stats::kmeans()
+  
+  kmobject <- kmeans(matrix,
+                     centers = K,
+                     iter.max = itermax,
+                     nstart = nstart)
+  
+  return(kmobject)
+  
+}
+
+# Linkage methods for hierarchical
+m <- c("average", "single", "complete", "ward")
+names(m) <- m
+
+
+hierarchical_check_methods <- function(x, df) {
+  # Takes a method and a dataframe, then returns the agglomerative coefficient
+  # resulting from that method performed on that dataframe. 
+  # --------------------------------------------------------------------------
+  # x - the method. "average", "single", etc.
+  # df - the dataframe. Should be like an expression matrix, and suitably
+  # scaled. 
+  
+  return(agnes(df, method = x)$ac)
+  
+}
+
+method_choice <- "ward"
+
+run_hierarchical <- function(matrix, method, K){
+  # Takes a matrix and a method, performs hierarchical clustering on it. 
+  # Returns the clustering call along with the resultant dendrogram, in a list.
+  # ---------------------------------------------------------------------------
+  # matrix - the expression matrix or similar. Should already be scaled etc. 
+  # method - method to pass to cluster::agnes. "average", "single", etc.
+  # K - number of centres to stats::cutree at, and return this partition.
+  
+  hobject <- agnes(matrix, method = method)
+  
+  dendrogram <- pltree(hobject,
+                       main = "Dendrogram",
+                       cex = 0.5,
+                       hang = -1)
+  
+  clustering <- cutree(as.hclust(hobject), k = K)
+  
+  return(list(
+    "hobject" = hobject,
+    "cluster" = clustering,
+    "dendrogram" = dendrogram
+  ))
+  
+  
+}
+
+evaluate_and_visualise <- function(matrix, labels, FUN, kmax = 20, b = 100) {
+  # Takes a partition of a dataset and computes several cluster evaluation 
+  # metrics: Silhouette score, Gap statistic, Variance Ratio Criterion. 
+  # -----------------------------------------------------------------------
+  # matrix - original value matrix that was clustered
+  # labels - partition
+  # FUN - cluster used to calc Gap, as it must be refit. 
+  
+  # Silhouette
+  whole_silhouette <- cluster::silhouette(labels, dist = dist(matrix))
+  avg_silhouette_width <- mean(whole_silhouette[, 3])
+  
+  # Calinski-Harabasz / VRC
+  calinhara <- fpc::calinhara(matrix, labels)
+  
+  # Gap - must refit to evaluate but seed is specified so this is valid. 
+  gap_stat <- cluster::clusGap(matrix,
+                               FUNcluster = FUN,
+                               K.max = kmax,
+                               B = b)$Tab[, 3]
+  
+  
+}
+
+
